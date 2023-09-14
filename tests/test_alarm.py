8d9757eacacca32384d32e8a699dacba8017b7d3
lukanny <vinicius_loureiro_cavalcante@hotmail.com>
2023-09-14 17:06:58 -0400

    Creating a bunch of tests.

--- tests/test_alarm.py
+++ tests/test_alarm.py
@@ -1,5 +1,25 @@
 import unittest
 
+from AlarmClock import AlarmClock
+
 class TestAlarm(unittest.TestCase):
 
-    pass
+    def setUp(self) -> None:
+        self.alarm = AlarmClock(
+            hours=1,
+            minutes=30,
+            seconds=0
+        )
+
+    def test_hours(self):
+        self.assertEqual(self.alarm.hours, 1)
+
+    def test_minutes(self):
+        self.assertEqual(self.alarm.minutes, 30)
+    
+    def test_seconds(self):
+        self.assertEqual(self.alarm.seconds, 0)
+
+    def test_alarm_is_not_none(self):
+        self.assertIsNotNone(self.alarm)
+        
