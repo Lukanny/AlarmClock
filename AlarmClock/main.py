c57d21d3a32289e0a7fee1373e8f6c0f87085223
lukanny <vinicius_loureiro_cavalcante@hotmail.com>
2023-09-14 18:46:29 -0400

    Adapting main.py to OOP paradigm.

--- AlarmClock/main.py
+++ AlarmClock/main.py
@@ -1,51 +1,19 @@
-from datetime import datetime, timedelta
-from AlarmClock.utils import send_message
+from datetime import datetime
+from alarm_clock import AlarmClock
+from AlarmClock import send_message
 import time
 
-alarm = input("New Alarm (HH:MM:SS): ")
 alarm_format = "%H:%M:%S"
-
+alarm = input("New Alarm (HH:MM:SS): ")
 alarm = datetime.strptime(alarm, alarm_format)
+alarm_clock = AlarmClock(hours=alarm.hour, minutes=alarm.minute, seconds=alarm.second, current=datetime.now())
+alarm_finished = alarm_clock.end_time()
 
-alarm_finished = timedelta(hours=alarm.hour, minutes=alarm.minute, 
-                           seconds=alarm.second) + datetime.now()
+print(f"Alarm duration: {alarm_clock}")
 
-print(f"See you at {alarm_finished.time()}")
+while not alarm_clock.alarm_has_ended():
+    time.sleep(5)
 
-while datetime.now().time() < alarm_finished.time():
-    time.sleep(60)
 
 send_message()
-
-
-class AlarmClock:
-
-    def __init__(self, **kwargs) -> None:
-        """
-        Class constructor with AlarmClock properties.
-
-        Common arguments:
-        'hours' - how many hours the alarm has
-        'minutes' - how many minutes the alarm has
-        'seconds' - how many seconds the alarm has
-        """
-        self._config = kwargs
-        if not self._config or len(self._config) == 0:
-            raise RuntimeError("A non null value is expected.")
-        
-    @property
-    def config(self):
-        return self._config
-    
-    @property
-    def hours(self):
-        return self._config.get("hours")
-    
-    @property
-    def minutes(self):
-        return self._config.get("minutes")
-    
-    @property
-    def seconds(self):
-        return self._config.get("seconds")
-    
+print("Alarm Ended.")
