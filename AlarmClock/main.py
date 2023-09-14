ec5ff59deadab7c4a6ac22d185342fa51ae1c540
lukanny <vinicius_loureiro_cavalcante@hotmail.com>
2023-09-14 17:05:48 -0400

    Adopting OOP paradigm and defining AlarmClock class with a few properties.

--- AlarmClock/main.py
+++ AlarmClock/main.py
@@ -16,3 +16,35 @@ while datetime.now().time() < alarm_finished.time():
     time.sleep(60)
 
 send_message()
+
+class AlarmClock:
+
+    def __init__(self, **kwargs) -> None:
+        """
+        Class constructor with AlarmClock properties.
+
+        Common arguments:
+        'hours' - how many hours the alarm has
+        'minutes' - how many minutes the alarm has
+        'seconds' - how many seconds the alarm has
+        """
+        self._config = kwargs
+        if not self._config or len(self._config == 0):
+            raise RuntimeError("A non null value is expected.")
+        
+    @property
+    def config(self):
+        return self._config
+    
+    @property
+    def hours(self):
+        return self._config.get("hours")
+    
+    @property
+    def minutes(self):
+        return self._config.get("minutes")
+    
+    @property
+    def seconds(self):
+        return self._config.get("seconds")
+    
